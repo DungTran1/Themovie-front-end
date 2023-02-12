2459fd5b195d9e96acd7be6241af95aa974b819f
DungTran1 <lssktt@gmail.com>
2023-02-12 09:38:36 +0700

    commit

--- src/service/axiosConfig.ts
+++ src/service/axiosConfig.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import apiConfig from "./apiConfig";
 
-const distance = axios.create({
+const instance = axios.create({
   baseURL: apiConfig.baseUrl,
   headers: {
     "content-type": "application/json",
   },
 });
-export const distance2 = axios.create({
+const instance2 = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "content-type": "application/json",
@@ -15,22 +15,15 @@ export const distance2 = axios.create({
 });
 
 const get = async (path: string, param = {}) => {
-  const res = await distance.get(path, {
+  const res = await instance.get(path, {
     params: { api_key: apiConfig.apiKey, ...param },
   });
   return res.data;
 };
-const getUser = async (path: string, config: any = {}) => {
-  const res = await distance2.get(
-    `${process.env.REACT_APP_API_URL}/${path}`,
-    config
-  );
-  return res.data;
-};
+
 const postUser = async (path: string, data: object, config: any = {}) => {
-  const res = await distance2.post(path, data, config);
+  const res = await instance2.post(path, data, config);
   return res.data;
 };
 
-export { get, getUser, postUser };
-export default distance;
+export { get, postUser };
