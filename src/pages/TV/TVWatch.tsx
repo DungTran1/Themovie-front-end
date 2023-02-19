5fc85160de150ee0c26501d4d57c9157c124aa8b
DungTran1 <lssktt@gmail.com>
2023-02-19 10:55:36 +0700

    commit

--- src/pages/TV/TVWatch.tsx
+++ src/pages/TV/TVWatch.tsx
@@ -16,10 +16,6 @@ const TVWatch: React.FC = () => {
 
   const seasonId = queryParams.get("season") || 1;
   const episodeId = queryParams.get("episode") || 1;
-  // if (error) return <div>ERROR: {error.message}</div>;
-  //   if (error) return <Error />;
-
-  // if (!data) return <div>Loading...</div>;
 
   const currentSeason = data?.detailSeasons?.find(
     (season) => season?.season_number === Number(seasonId)
@@ -28,11 +24,6 @@ const TVWatch: React.FC = () => {
   const currentEpisode = currentSeason?.episodes.find(
     (episode) => episode?.episode_number === Number(episodeId)
   );
-
-  console.log(data, seasonId, episodeId, currentSeason, currentEpisode);
-  // I check data is truthy because I want to show 404 only when invalid episode or season are accessed, NOT when data is fetching
-  // if (!currentEpisode && data) return <div>ERROR: 404</div>;
-  //   if (!currentEpisode && data) return <Error />;
 
   return (
     <FilmWatch
