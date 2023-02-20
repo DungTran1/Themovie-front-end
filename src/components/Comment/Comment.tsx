16a70cc1f26b5c9bb741c578953bdd168e1c7dbd
DungTran1 <lssktt@gmail.com>
2023-02-20 17:31:08 +0700

    commit

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -26,7 +26,11 @@ const Comment: React.FC<CommentProp> = ({ movieId }) => {
   const [commentOption, setCommentOption] = useState("lastest");
   const { data, refetch } = useQuery<{ comment: UserComment[] }>(
     ["comment", movieId, user],
-    () => postUser("comment", { movieId: movieId })
+    () => postUser("comment", { movieId: movieId }),
+    {
+      refetchInterval: 1000,
+      staleTime: 1000,
+    }
   );
   const [numberOfComments, setNumberOfComments] = useState<number>(5);
 
